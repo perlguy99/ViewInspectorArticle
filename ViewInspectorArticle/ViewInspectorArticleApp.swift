a0a92fc6e827cdbfb989e340cdc5c764d72f65f7
Brent Michalski <Brent.Michalski@gmail.com>
2024-07-22 14:06:32 -0500

    Initial Commit

---
+++ ViewInspectorArticle/ViewInspectorArticleApp.swift
@@ -0,0 +1,17 @@
+//
+//  ViewInspectorArticleApp.swift
+//  ViewInspectorArticle
+//
+//  Created by Brent Michalski on 7/22/24.
+//
+
+import SwiftUI
+
+@main
+struct ViewInspectorArticleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
