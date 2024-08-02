7029a3a798000a545791a22b64ba708a5e66b777
Brent Michalski <Brent.Michalski@gmail.com>
2024-08-02 16:52:30 -0500

    * f Added GameController and initial test

---
+++ ViewInspectorArticle/GameController.swift
@@ -0,0 +1,12 @@
+//
+//  GameController.swift
+//  ViewInspectorArticle
+//
+//  Created by Brent Michalski on 8/2/24.
+//
+
+import Foundation
+
+class GameController {
+    
+}
---
+++ ViewInspectorArticleTests/GameControllerTests.swift
@@ -0,0 +1,19 @@
+//
+//  GameControllerTests.swift
+//  ViewInspectorArticle
+//
+//  Created by Brent Michalski on 8/2/24.
+//
+@testable import ViewInspectorArticle
+import ViewInspector
+import XCTest
+
+final class GameControllerTests: XCTestCase {
+    
+    func testGameControllerCreation() throws {
+        XCTAssertNoThrow(GameController())
+   }
+
+    
+    
+}
