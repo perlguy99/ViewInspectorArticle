a0db440a80bae7b551e671e4d4e36a9c305c41f9
Brent Michalski <Brent.Michalski@gmail.com>
2024-08-03 15:07:35 -0500

    * f Added checks for winning game, still need refactoring

--- ViewInspectorArticle/GameController.swift
+++ ViewInspectorArticle/GameController.swift
@@ -38,21 +38,128 @@ struct Game {
 
 @Observable
 class GameController {
+    var game: Game = Game()
+    
     var turn: TTT_Turn = .x
+
+    func toggleTurn() {
+        turn = (turn == .x ? .o : .x)
+    }
     
-//    var newGame = Game()
-//    
-//    var gameSquares = [
-//        TTTSquare(), TTTSquare(), TTTSquare(),
-//        TTTSquare(), TTTSquare(), TTTSquare(),
-//        TTTSquare(), TTTSquare(), TTTSquare()
-//    ]
+    func resetGame() {
+        turn = .x
+    }
+    
+    func newGame() {
+        
+    }
+    
+    func foo() {
+        print(game.squares[0].value)
+    }
     
     
-//    var game = [
-//        TTTSquareView(viewId: 0, square: gameSquares[0]), TTTSquareView(viewId: 1, square: <#TTTSquare#>), TTTSquareView(viewId: 2, square: <#TTTSquare#>),
-//        TTTSquareView(viewId: 3, square: <#TTTSquare#>), TTTSquareView(viewId: 4, square: <#TTTSquare#>), TTTSquareView(viewId: 5, square: <#TTTSquare#>),
-//        TTTSquareView(viewId: 6, square: <#TTTSquare#>), TTTSquareView(viewId: 7, square: <#TTTSquare#>), TTTSquareView(viewId: 8, square: <#TTTSquare#>)
-//    ]
+    func toggleSquare(index: Int) {
+        guard index >= 0 && index <= 8 else {
+            print("Index out of range!")
+            return
+        }
+        
+        game.squares[index].toggle()
+    }
+    
+    func takeTurn(index: Int) {
+        guard index >= 0 && index <= 8 else {
+            print("Index out of range!")
+            return
+        }
+        
+        // Verify square has not been tapped yet
+        if game.squares[index].value == .empty {
+            // Check to see who's turn it is
+            switch turn {
+            case .x:
+                game.squares[index].value = .x
+            case .o:
+                game.squares[index].value = .o
+            }
+            
+            toggleTurn()
+        }
+    }
+    
+    func checkForWin() -> String? {
+        let x_wins = "X Won!"
+        let o_wins = "O Wins the Game!"
+        let draw = "It's a Draw!"
+        
+        // X0 O1 X2
+        // O3 X4 O5
+        // X6 O7 X8
+        
+        // if 0, 1, 2 same = (win)
+        // if 3, 4, 5 same = (win)
+        // if 6, 7, 8 same = (win)
+        
+        // if 0, 3, 6 same = (win)
+        // if 1, 4, 7 same = (win)
+        // if 2, 5, 8 same = (win)
+        
+        // if 0, 4, 8 same = (win)
+        // if 2, 4, 6 same = (win)
+        
+        if game.squares[0].value == .x && game.squares[1].value == .x && game.squares[2].value == .x {
+            return x_wins
+        }
+
+        if game.squares[0].value == .o && game.squares[1].value == .o && game.squares[2].value == .o {
+            return o_wins
+        }
+
+        if game.squares[3].value == .x && game.squares[4].value == .x && game.squares[5].value == .x {
+            return x_wins
+        }
+
+        if game.squares[3].value == .o && game.squares[4].value == .o && game.squares[5].value == .o {
+            return o_wins
+        }
+
+        if game.squares[6].value == .x && game.squares[7].value == .x && game.squares[8].value == .x {
+            return x_wins
+        }
+
+        if game.squares[6].value == .o && game.squares[7].value == .o && game.squares[8].value == .o {
+            return o_wins
+        }
+
+        if game.squares[0].value == .x && game.squares[4].value == .x && game.squares[8].value == .x {
+            return x_wins
+        }
+
+        if game.squares[0].value == .o && game.squares[4].value == .o && game.squares[8].value == .o {
+            return o_wins
+        }
+
+        if game.squares[2].value == .x && game.squares[4].value == .x && game.squares[6].value == .x {
+            return x_wins
+        }
+
+        if game.squares[2].value == .o && game.squares[4].value == .o && game.squares[6].value == .o {
+            return o_wins
+        }
+
+        // if ALL are filled and still no winner, then it is a draw!
+        for square in game.squares {
+            // are there any empties?
+            if square.value == .empty {
+                return nil
+            } else {
+                return draw
+            }
+        }
+        
+        return nil
+    }
+    
     
 }
--- ViewInspectorArticleTests/GameControllerTests.swift
+++ ViewInspectorArticleTests/GameControllerTests.swift
@@ -10,16 +10,233 @@ import XCTest
 
 final class GameControllerTests: XCTestCase {
     
-    func testGameControllerCreation() throws {
+    func testCreation() throws {
         XCTAssertNoThrow(GameController())
-   }
+    }
     
-    func testGameControllerStartsWithTurnX() {
+    func testStartsWithTurnX() {
+        let sut = GameController()
+        XCTAssertEqual(sut.turn, .x)
+    }
+    
+    func testToggleturnWorks() {
+        let sut = GameController()
+        XCTAssertEqual(sut.turn, .x)
+        sut.toggleTurn()
+        
+        XCTAssertEqual(sut.turn, .o)
+        sut.toggleTurn()
+        XCTAssertEqual(sut.turn, .x)
+        sut.toggleTurn()
+        XCTAssertEqual(sut.turn, .o)
+    }
+    
+    func testResetGame() {
         let sut = GameController()
         
         XCTAssertEqual(sut.turn, .x)
+        sut.toggleTurn()
+        XCTAssertEqual(sut.turn, .o)
+        
+        sut.resetGame()
+        XCTAssertEqual(sut.turn, .x)
+    }
+    
+    func testThatGameHasExactlyNineSquares() {
+        let sut = Game()
+        XCTAssertEqual(sut.squares.count, 9)
+    }
+    
+    func testTakeTurn() {
+        let sut = GameController()
+        
+        // Verify all are .empty
+        XCTAssertEqual(sut.game.squares[0].value, .empty)
+        XCTAssertEqual(sut.game.squares[1].value, .empty)
+        XCTAssertEqual(sut.game.squares[2].value, .empty)
+        XCTAssertEqual(sut.game.squares[3].value, .empty)
+        XCTAssertEqual(sut.game.squares[4].value, .empty)
+        XCTAssertEqual(sut.game.squares[5].value, .empty)
+        XCTAssertEqual(sut.game.squares[6].value, .empty)
+        XCTAssertEqual(sut.game.squares[7].value, .empty)
+        XCTAssertEqual(sut.game.squares[8].value, .empty)
+        
+        // First lets see if we can't simple toggle each one.
+        sut.takeTurn(index: 4)
+        XCTAssertEqual(sut.game.squares[4].value, .x)
+        
+        sut.takeTurn(index: 5)
+        XCTAssertEqual(sut.game.squares[5].value, .o)
+    }
+    
+    func testTakeTurnCannotPickSameSquare() {
+        let sut = GameController()
+        
+        // Verify empty
+        XCTAssertEqual(sut.game.squares[4].value, .empty)
+        
+        // First lets see if we can't simple toggle each one.
+        sut.takeTurn(index: 4)
+        XCTAssertEqual(sut.game.squares[4].value, .x)
+        
+        sut.takeTurn(index: 4)
+        XCTAssertEqual(sut.game.squares[4].value, .x)
+    }
+    
+    func testTakeTurnUntilBoardFull() {
+        let sut = GameController()
+        
+        // Test we can fill the board and keep track of turns
+        sut.takeTurn(index: 0)
+        XCTAssertEqual(sut.game.squares[0].value, .x)
+        
+        sut.takeTurn(index: 1)
+        XCTAssertEqual(sut.game.squares[1].value, .o)
+        
+        sut.takeTurn(index: 2)
+        XCTAssertEqual(sut.game.squares[2].value, .x)
+        
+        sut.takeTurn(index: 3)
+        XCTAssertEqual(sut.game.squares[3].value, .o)
+        
+        sut.takeTurn(index: 4)
+        XCTAssertEqual(sut.game.squares[4].value, .x)
+        
+        sut.takeTurn(index: 5)
+        XCTAssertEqual(sut.game.squares[5].value, .o)
+        
+        sut.takeTurn(index: 6)
+        XCTAssertEqual(sut.game.squares[6].value, .x)
+        
+        sut.takeTurn(index: 7)
+        XCTAssertEqual(sut.game.squares[7].value, .o)
+        
+        sut.takeTurn(index: 8)
+        XCTAssertEqual(sut.game.squares[8].value, .x)
+    }
+    
+    // 3 in-a-row wins
+    // 3 across, 1, 2, 3
+    // 3 down, 1, 2, 3
+    // 3 diagonally 1, 2
+    let expected_x_wins = "X Won!"
+    let expected_o_wins = "O Wins the Game!"
+    let expected_draw = "It's a Draw!"
+    
+    
+    func testBoardForWin_x_across_top() {
+        let sut = GameController()
+        
+        sut.game.squares[0].value = .x
+        sut.game.squares[1].value = .x
+        sut.game.squares[2].value = .x
+        
+        XCTAssertEqual(sut.checkForWin(), expected_x_wins)
+    }
+    
+    func testBoardForWin_o_across_top() {
+        let sut = GameController()
+        
+        sut.game.squares[0].value = .o
+        sut.game.squares[1].value = .o
+        sut.game.squares[2].value = .o
+        
+        XCTAssertEqual(sut.checkForWin(), expected_o_wins)
+    }
+    
+    func testBoardForWin_x_across_middle() {
+        let sut = GameController()
+        
+        sut.game.squares[3].value = .x
+        sut.game.squares[4].value = .x
+        sut.game.squares[5].value = .x
+        
+        XCTAssertEqual(sut.checkForWin(), expected_x_wins)
+    }
+    
+    func testBoardForWin_o_across_middle() {
+        let sut = GameController()
+        
+        sut.game.squares[3].value = .o
+        sut.game.squares[4].value = .o
+        sut.game.squares[5].value = .o
+        
+        XCTAssertEqual(sut.checkForWin(), expected_o_wins)
+    }
+    
+    func testBoardForWin_x_across_bottom() {
+        let sut = GameController()
+        
+        sut.game.squares[3].value = .x
+        sut.game.squares[4].value = .x
+        sut.game.squares[5].value = .x
+        
+        XCTAssertEqual(sut.checkForWin(), expected_x_wins)
+    }
+    
+    func testBoardForWin_o_across_bottom() {
+        let sut = GameController()
+        
+        sut.game.squares[3].value = .o
+        sut.game.squares[4].value = .o
+        sut.game.squares[5].value = .o
+        
+        XCTAssertEqual(sut.checkForWin(), expected_o_wins)
+    }
+    
+    func testBoardForWin_x_across_diagonal1() {
+        let sut = GameController()
+        
+        sut.game.squares[0].value = .x
+        sut.game.squares[4].value = .x
+        sut.game.squares[8].value = .x
+        
+        XCTAssertEqual(sut.checkForWin(), expected_x_wins)
+    }
+    
+    func testBoardForWin_o_across_diagonal1() {
+        let sut = GameController()
+        
+        sut.game.squares[0].value = .o
+        sut.game.squares[4].value = .o
+        sut.game.squares[8].value = .o
+        
+        XCTAssertEqual(sut.checkForWin(), expected_o_wins)
     }
 
-    
-    
+    func testBoardForWin_x_across_diagonal2() {
+        let sut = GameController()
+        
+        sut.game.squares[2].value = .x
+        sut.game.squares[4].value = .x
+        sut.game.squares[6].value = .x
+        
+        XCTAssertEqual(sut.checkForWin(), expected_x_wins)
+    }
+
+    func testBoardForWin_o_across_diagonal2() {
+        let sut = GameController()
+        
+        sut.game.squares[2].value = .o
+        sut.game.squares[4].value = .o
+        sut.game.squares[6].value = .o
+        
+        XCTAssertEqual(sut.checkForWin(), expected_o_wins)
+    }
+
+    func testBoardForDraw() {
+        let sut = GameController()
+        
+        sut.game.squares[0].value = .x
+        sut.game.squares[1].value = .x
+        sut.game.squares[2].value = .o
+        sut.game.squares[3].value = .o
+        sut.game.squares[4].value = .o
+        sut.game.squares[5].value = .x
+        sut.game.squares[6].value = .x
+        sut.game.squares[7].value = .o
+        sut.game.squares[8].value = .x
+        
+        XCTAssertEqual(sut.checkForWin(), expected_draw)
+    }
 }
