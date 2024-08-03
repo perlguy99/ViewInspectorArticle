e4ed50ac01d6cb2b712f6d55f11b0a9ef8c33923
Brent Michalski <Brent.Michalski@gmail.com>
2024-08-03 17:40:36 -0500

    . d Just cleaned up a bit

--- ViewInspectorArticle/TTTSquareView.swift
+++ ViewInspectorArticle/TTTSquareView.swift
@@ -11,22 +11,14 @@ struct TTTSquareView: TestableView {
     var viewInspectorHook: ((TTTSquareView) -> Void)?
     var viewId: Int = -1
 
-    @Binding var square: TTTSquare {
-        didSet {
-            print("\n------------TTTSquare.didSet------------------")
-            print(square.stringValue)
-            print("--------------TTTSquare.didSet----------------\n")
-        }
-    }
+    @Binding var square: TTTSquare
 
     var body: some View {
         RoundedRectangle(cornerRadius: 8)
             .fill(Color.teal).opacity(0.5)
             .frame(width: 100, height: 100)
             .overlay {
-                let _ = print("\n--------------overlay----------------\n")
                 if let image = square.image {
-                    let _ = print("\n--------------image----------------\n")
                     image
                         .resizable()
                         .frame(width: 75, height: 75)
@@ -38,11 +30,7 @@ struct TTTSquareView: TestableView {
     }
     
     func handleOnTapGesture() {
-        print("\n------------------------------")
-        print(square.stringValue as Any)
         square.toggle()
-        print("TTTSquareView onTapGesture was called on viewID: \(viewId), \(String(describing: square.stringValue)).")
-        print("------------------------------\n")
     }
 }
 
