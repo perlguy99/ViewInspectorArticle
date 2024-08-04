a6243fef29a4728f6c902ca7150773a8918f99a3
Brent Michalski <Brent.Michalski@gmail.com>
2024-08-04 15:51:33 -0500

    Added a GameController EnvironmentObject

--- ViewInspectorArticle/ViewInspectorArticleApp.swift
+++ ViewInspectorArticle/ViewInspectorArticleApp.swift
@@ -9,9 +9,12 @@ import SwiftUI
 
 @main
 struct ViewInspectorArticleApp: App {
+    @StateObject var gameController = GameController()
+    
     var body: some Scene {
         WindowGroup {
             ContentView()
+                .environmentObject(gameController)
         }
     }
 }
